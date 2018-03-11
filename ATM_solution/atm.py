289fe843e09fdc63cba31957a703244d06462915
Unknown <aemam383@gmail.com>
2018-03-11 11:27:02 +0400

    improving code

--- ATM_solution/atm.py
+++ ATM_solution/atm.py
@@ -1,19 +1,27 @@
-money = 500
-req = 277
+def atm(mony,req):
+    if mony >=req:
+        while req > 0:
+            if req >= 100:
+                print "give 100"
+                req = req - 100
+            elif req >= 50:
+                print "give 50"
+                req = req - 50
+            elif req >= 10:
+                print "give 10"
+                req = req - 10
+            elif req >= 5:
+                print "give 5"
+                req = req - 5
+            elif req >= 2:
+                print "give 2"
+                req = req - 2
+            elif req >=1:
+                print "give 1"
+                req=req-1
+    else:
+        print "not avalable "
 
-while req >0:
-    if req >=100:
-        print "give 100"
-        req=req-100
-    elif req >=50:
-        print "give 50"
-        req=req-50
-    elif req >=10:
-        print "give 10"
-        req=req-10
-    elif req >=5:
-        print "give 5"
-        req=req-5
-    elif req >=2:
-        print "give 2"
-        req=req-2
+atm(500,88)
+
+
