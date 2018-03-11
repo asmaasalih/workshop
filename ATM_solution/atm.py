ca5e3e3c78f0c7a0f370ff16a0247acf8b6f21de
Unknown <aemam383@gmail.com>
2018-03-11 11:07:11 +0400

    add atm code

---
+++ ATM_solution/atm.py
@@ -0,0 +1,19 @@
+money = 500
+req = 277
+
+while req >0:
+    if req >=100:
+        print "give 100"
+        req=req-100
+    elif req >=50:
+        print "give 50"
+        req=req-50
+    elif req >=10:
+        print "give 10"
+        req=req-10
+    elif req >=5:
+        print "give 5"
+        req=req-5
+    elif req >=2:
+        print "give 2"
+        req=req-2
