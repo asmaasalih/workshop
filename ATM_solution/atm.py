564a300cd6466e385ad449b277e8b4ee1229eb4c
Unknown <aemam383@gmail.com>
2018-03-12 09:44:34 +0400

    improving code

--- ATM_solution/atm.py
+++ ATM_solution/atm.py
@@ -1,5 +1,12 @@
-def atm(mony,req):
-    if mony >=req:
+
+def withdraw(balance,req):
+    if req>balance:
+        print "Can't give you all this money !!"
+    elif req<=0:
+        print("More than zero plz!")
+    
+    if balance >=req:
+        balance =balance - req
         while req > 0:
             if req >= 100:
                 print "give 100"
@@ -19,11 +26,17 @@ def atm(mony,req):
             elif req >=1:
                 print "give 1"
                 req=req-1
-    else:
-        print "not avalable "
-    if req <0:
-        print "try positive value"
-
-atm(500,-154)
-
+    
+    return balance - req
+    
+balance=500
+print "current balance : "+str(balance)
+balance=withdraw(balance, 277)
+print "current balance: "+str(balance)
+balance=withdraw(balance, 165)
+print "current balance: "+str(balance)
+balance=withdraw(balance, 30)
+print "current balance: "+str(balance)
+balance=withdraw(balance, 133)
+print "current balance: "+str(balance)
 
