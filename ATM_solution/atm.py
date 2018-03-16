0ae078ae0474ab5654d7502969661db1d713ca2c
Unknown <aemam383@gmail.com>
2018-03-16 07:54:24 +0400

    adding withdrawals_list

--- ATM_solution/atm.py
+++ ATM_solution/atm.py
@@ -2,11 +2,16 @@ class ATM:
     def __init__(self,bank_name,balance):
         self.bank_name=bank_name
         self.balance=balance
+        self.withdrawals_list = []
+
+    def show_withdrawals(self):
+        for withdrawal in self.withdrawals_list:
+            print(withdrawal)    
+    
 
     def withdraw(self,req):
         print "Welcome to "+self.bank_name
         print "current balance : "+str(self.balance)
-
         if req>self.balance:
             print "Can't give you all this money !!"
 
@@ -14,20 +19,21 @@ class ATM:
             print "Inter positive number only please!! "    
         
         if self.balance >=req:
-            self.balance=self.balance-req
+            self.withdrawals_list.append(req)
+            self.balance = self.balance-req
             while req > 0:
                 if req >= 100:
                     print "give 100"
-                    req = req - 100
+                    req =req-100
                 elif req >= 50:
                     print "give 50"
-                    req = req - 50
+                    req =req-50
                 elif req >= 10:
                     print "give 10"
-                    req = req - 10
+                    req =req-10
                 elif req >= 5:
                     print "give 5"
-                    req = req - 5
+                    req =req-5
                 elif req >= 2:
                     print "give 2"
                     req = req - 2
@@ -37,7 +43,7 @@ class ATM:
         
         
     
-balance1 = 500
+balance1 = 5000
 balance2 = 1000
 
 
@@ -47,6 +53,12 @@ atm2=ATM("Islamic bank",balance2)
 
 atm1.withdraw(277)
 atm1.withdraw(800)
+atm1.withdraw(300)
+atm1.withdraw(194)
+print atm1.withdrawals_list
 
 atm2.withdraw(100)
-atm2.withdraw(2000)
+atm2.withdraw(200)
+atm2.withdraw(105)
+atm2.withdraw(88)
+print atm2.withdrawals_list
