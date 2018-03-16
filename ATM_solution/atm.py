546c7d522154f8fecac7093077c24d3ade1192c4
Unknown <aemam383@gmail.com>
2018-03-16 12:27:09 +0400

    add function process_request

--- ATM_solution/atm.py
+++ ATM_solution/atm.py
@@ -7,18 +7,8 @@ class ATM:
     def show_withdrawals(self):
         for withdrawal in self.withdrawals_list:
             print(withdrawal)    
-    
 
-    def withdraw(self,req):
-        print "Welcome to "+self.bank_name
-        print "current balance : "+str(self.balance)
-        if req>self.balance:
-            print "Can't give you all this money !!"
-
-        if req<=0:
-            print "Inter positive number only please!! "    
-        
-        if self.balance >=req:
+    def process_request(self,req):    
             self.withdrawals_list.append(req)
             self.balance = self.balance-req
             while req > 0:
@@ -39,9 +29,23 @@ class ATM:
                     req = req - 2
                 elif req >=1:
                     print "give 1"
-                    req=req-1
-        
-        
+                    req=req-1    
+
+    def withdraw(self,req):
+        print '=' * 25
+        print "Welcome to "+self.bank_name
+        print "current balance : "+str(self.balance)
+        print '=' * 25
+        if req>self.balance:
+            print "Can't give you all this money !!"
+
+        if req<=0:
+            print "Inter positive number only please!! "    
+        else:
+
+            self.process_request(req)
+            
+            
     
 balance1 = 5000
 balance2 = 1000
@@ -52,13 +56,13 @@ atm2=ATM("Islamic bank",balance2)
 
 
 atm1.withdraw(277)
-atm1.withdraw(800)
-atm1.withdraw(300)
-atm1.withdraw(194)
+atm1. withdraw(800)
+atm1. withdraw(300)
+atm1. withdraw(194)
 print atm1.withdrawals_list
 
-atm2.withdraw(100)
-atm2.withdraw(200)
-atm2.withdraw(105)
-atm2.withdraw(88)
+atm2. withdraw(100)
+atm2. withdraw(200)
+atm2. withdraw(105)
+atm2. withdraw(88)
 print atm2.withdrawals_list
